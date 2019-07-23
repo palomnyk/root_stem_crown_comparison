569401960b4f1f3cd1fe063254d70ac162142217
Aaron_Y <amyerke@uncc.edu>
2019-07-23 10:46:33 -0400

    trying something out

--- lmFiguresCombinDatasets.R
+++ lmFiguresCombinDatasets.R
@@ -63,103 +63,63 @@ lcNeg = read.table(
 datasets = list(gc, lcPos, lcNeg)
 datasetNames = c('GC/MS', 'LC/MS+', 'LC/MS-')
 
-lmMetadata = function(testingData, label){
-  
-  index = c()
-  metaName = c()
-  metaPval = c()
-  indexName = c()
-  r_sqrd = c()
-  
-  for (i1 in 1:nrow(testingData)){
-    for (i2 in 1:nrow(metadata)){
-      tryCatch(
-        { 
-          myLm = lm( unlist(testingData[i1,]) ~ as.factor(as.character(metadata[i2,])))
-          index = c(index, i1)
-          metaName = c(metaName, row.names(metadata)[i2])
-          indexName = c(indexName, row.names(testingData)[i1])
-          metaPval =  c(metaPval, anova(myLm)$"Pr(>F)"[1])
-          r_sqrd =  c(r_sqrd, summary(myLm)$r.squared)
-        },
-        error=function(cond) {
-          print('an error is thrown')
-          message(cond)
-        },
-        warning=function(cond) {
-          print('a warning is thrown')
-          message(cond)
-          # Choose a return value in case of warning
-          #return(NULL)
-        }
-      )
-    }
-  }
-  
-  pdf(paste(ds, "HistogramRsqrdMetadata", label, "Lm.pdf", sep=""))
-  hist(r_sqrd, 
-       main = paste(ds, ' Histogram of lm r squared ', label),
-       xlab = 'Adjusted R squared',
-       breaks = 50
-  )
-  dev.off()
-  
-  pdf(paste(ds, "HistogramPvalMetaVs", label, "Lm.pdf", sep=""))
-  hist(metaPval, 
-       main = paste(ds, ' Histogram of lm pvalues for ', label),
-       xlab = 'Uncorrected pvalues',
-       breaks = 50
-  )
-  dev.off()
-  
-  metaPvalAdj = p.adjust(metaPval, method = "BH")
-  dFrame <- data.frame(metaPvalAdj,metaName,index, indexName)
-  dFrame <- dFrame [order(dFrame$metaPvalAdj),]
-  
-  write.table(dFrame, file=paste(ds, "MetaVs",label,"LinearMod.txt", sep = ''), row.names=FALSE, sep="\t")
-  
-  write.table(format(dFrame, digits = 3), file=paste(ds, "MetaVs",label,"ShortLinearMod.txt", sep = ''), row.names=FALSE, sep="\t")
-  
-  pubDf = reshape(dFrame, idvar = 'indexName', timevar = 'metaName', direction="wide", sep="", drop = "index")
-  write.table(format(pubDf, digits = 3), file=paste(ds, "MetaVs",label,"ReformatShortLinearMod.txt", sep = ''), row.names=FALSE, sep="\t")
-  
-  #print(pubDf)
-  
-  pdf(paste(ds,"MetaVs",label,"LinearMod.pdf", sep = ""))
-  
-  
-  for( i in 1:nrow(dFrame)){
-    aTitle <- paste(label, ': ', dFrame$indexName[i], " vs ",  dFrame$metaName[i], "\nAdj Pval: ", formatC(dFrame$metaPvalAdj[i]), sep = '')
-    
-    par(bty = 'l', 
-        mar = c(5, 4, 5, 2) + 0.1)
-    plot( as.factor(as.character(metadata[as.character(dFrame$metaName[i]),])),
-          unlist(testingData[dFrame$index[i],]),
-          main = aTitle,
-          xlab = as.character(dFrame$metaName[i]),
-          ylab = paste(as.character(dFrame$indexName[i])),
-          col = brewer.pal(length(metadata[as.character(dFrame$metaName[i]),]),"Accent")
-    )
-  }
-  dev.off()
-  
-  print('lmMetadata complete')
-}#end of lmMetadata
-
 
 for (lab in row.names(metadata)){
   print(lab)
+  print(as.factor(as.character(metadata[lab,])))
   
+  pdf(file.path(getwd(), 'figures', paste( lab, "LinearModFigure.pdf", sep = "")))
+  par(par(mfrow=c(1,3)))
   
   for(ds in 1:length(datasets)){
     print(datasetNames[ds])
     
-    pdf(path.join(getwd(), 'figures', paste(datasetNames[ds], lab, "LinearModFigure.pdf", sep = "")))
-    par(par(mfrow=c(1,3)))
+    testingData = data.frame(datasets[ds])
     
-            myLm = lm( testingData ~ as.factor(as.character(metadata[lab,])))
-            metaPval =  c(metaPval, anova(myLm)$"Pr(>F)"[1])
+    catagories = unique(as.character(metadata[lab,]))
+    
+    testingData[1,]
+    
+    myRow = as.vector(testingData[1, ])
+  
+    # It has to be a flattened df -R's nested loops simply wont do it.
+    # column 1 == values, column 2 == metadata catagory
+    # possible 3rd column == metabolite for bioactives (after subsetting)
+    
+    lists = list()
+    listLevels = levels(as.factor(metadata[lab,]))
+    metaFact = as.factor(metadata[lab,])
+    metaVect = as.vector(metadata[lab,])
+    testDim = dim(testingData)
+    dfLength = testDim[1]*testDim[2]
+    flatDf = data.frame(numeric(dfLength), character(dfLength))
+    
+    for (lev in length(listLevels)) {
+      print(lev)
+      lists[lev] = list(0)
+    }
+    for(i in 1:dim(testingData)[2]){
+      print(match(metaVect[i], listLevels))
+      print(length(testingData[,i]))
+      index = match(metaVect[i], listLevels)
+      append(lists[[index]], testingData[,i])
+    }
+    newDf = as.data.frame(lists())
 
+    myLm = lm( testingData ~ as.factor(as.character(metadata[lab,])))
+    metaPval =  c(metaPval, anova(myLm)$"Pr(>F)"[1])
+    
+    aTitle <- paste(label, ': ', dFrame$indexName[i], " vs ",  dFrame$metaName[i], "\nAdj Pval: ", formatC(dFrame$metaPvalAdj[i]), sep = '')
+    
+    par(bty = 'l', 
+        mar = c(5, 4, 5, 2) + 0.1)
+    plot( as.factor(as.character(metadata[as.character(metadata[lab]),])),
+          testingData,
+          main = aTitle,
+          xlab = as.character(lab),
+          ylab = paste(as.character(datasets[ds])),
+          #col = brewer.pal(length(metadata[as.character(lab),]),"Accent")
+    )
     
   }#end datasets loop
   
